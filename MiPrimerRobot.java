a5ef5a54007d5fe429a9061ebba7f745054c029c
Davidcuama <74799027+Davidcuama@users.noreply.github.com>
2024-05-19 20:42:49 -0500

    Movimiento de Robots ( Recoger beepers e intento de llevar a ubicación)

--- MiPrimerRobot.java
+++ MiPrimerRobot.java
@@ -5,39 +5,222 @@ import java.util.List;
 
 class Racer extends Robot {
     private int maxBeepers;
-    private int deliveryPosition[];
+    private int[] deliveryPosition;
+    private int currentStreet;
+    private int currentAvenue;
+    private int startStreet;
+    private int startAvenue;
+    private static List<int[]> beeperLocations = new ArrayList<>();
+
     public Racer(int street, int avenue, Direction direction, int beepers, Color color, int maxBeepers) {
         super(street, avenue, direction, beepers, color);
         this.maxBeepers = maxBeepers;
-        this.deliveryPosition = new int[2];
-        this.deliveryPosition[0] = street;
-        this.deliveryPosition[1] = maxBeepers; //aveneu para entregar los beeper
+        this.deliveryPosition = new int[]{maxBeepers, 1}; // Calle en la posición 0 y la avenida en la posición 1
+        this.currentStreet = street;
+        this.currentAvenue = avenue;
+        this.startStreet = street;
+        this.startAvenue = avenue;
         World.setupThread(this);
     }
 
     public void race() {
-        System.out.print(anyBeepersInBeeperBag());
-        // putBeeper();
-        // move();
-        // turnOff();
+        while (true) {
+            int beepersCollected = 0;
+            boolean foundBeepers = false;
+
+            // Recoger beepers hasta alcanzar la capacidad máxima o no encontrar más beepers
+            while (beepersCollected < maxBeepers && !outOfBoundaries()) {
+                if (nextToABeeper()) {
+                    pickBeeper();
+                    beepersCollected++;
+                    System.out.println("Robot at (" + currentStreet + ", " + currentAvenue + ") picked up a beeper.");
+                    foundBeepers = true;
+                    updateBeeperLocations(currentStreet, currentAvenue);
+                }
+
+                if (beepersCollected < maxBeepers) {
+                    if (frontIsClear() && !nextToARobot()) {
+                        move();
+                        updatePosition();
+                    } else {
+                        if (!turnToClearDirection()) {
+                            break; // Si no hay ninguna dirección clara, termina la carrera
+                        }
+                    }
+                }
+            }
+
+            if (beepersCollected > 0) {
+                // Llevar los beepers a la ubicación designada
+                deliverBeepers();
+
+                // Volver a la posición inicial
+                returnToStart();
+            } else if (!foundBeepers) {
+                // Si no se encontraron más beepers, terminar la carrera
+                returnToStart();
+                turnOff();
+                break;
+            }
+        }
+    }
+
+    public void deliverBeepers() {
+        System.out.println("Heading to deliver beepers to (" + deliveryPosition[0] + ", " + deliveryPosition[1] + ")");
+        moveToLocation(deliveryPosition[0], deliveryPosition[1]);
+        while (anyBeepersInBeeperBag()) {
+            putBeeper();
+            System.out.println("Delivered a beeper to (" + deliveryPosition[0] + ", " + deliveryPosition[1] + ")");
+        }
+    }
+
+    public void returnToStart() {
+        System.out.println("Returning to start at (" + startStreet + ", " + startAvenue + ")");
+        moveToLocation(startStreet, startAvenue);
+        System.out.println("Returned to start at (" + startStreet + ", " + startAvenue + ")");
+    }
+
+    private boolean outOfBoundaries() {
+        return currentStreet < 1 || currentStreet > 8 || currentAvenue < 1 || currentAvenue > 10;
+    }
+
+    private boolean turnToClearDirection() {
+        for (int i = 0; i < 4; i++) {
+            turnLeft();
+            if (frontIsClear() && !nextToARobot()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void moveToStreet(int targetStreet) {
+        while (currentStreet != targetStreet) {
+            if (currentStreet < targetStreet) {
+                while (!facingNorth()) {
+                    turnLeft();
+                }
+            } else if (currentStreet > targetStreet) {
+                while (!facingSouth()) {
+                    turnLeft();
+                }
+            }
+
+            if (frontIsClear()) {
+                move();
+                updatePosition();
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void moveToAvenue(int targetAvenue) {
+        while (currentAvenue != targetAvenue) {
+            if (currentAvenue < targetAvenue) {
+                while (!facingEast()) {
+                    turnLeft();
+                }
+            } else if (currentAvenue > targetAvenue) {
+                while (!facingWest()) {
+                    turnLeft();
+                }
+            }
+
+            if (frontIsClear()) {
+                move();
+                updatePosition();
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void moveToLocation(int targetStreet, int targetAvenue) {
+        moveToStreet(targetStreet);
+        moveToAvenue(targetAvenue);
+    }
+
+    private boolean anyBeepersInWorld() {
+        synchronized (beeperLocations) {
+            return !beeperLocations.isEmpty();
+        }
+    }
+
+    private void updateBeeperLocations(int street, int avenue) {
+        synchronized (beeperLocations) {
+            int[] currentLocation = {street, avenue};
+            beeperLocations.removeIf(location -> location[0] == street && location[1] == avenue && !nextToABeeper());
+        }
+    }
+
+    public static void addBeeperLocation(int street, int avenue) {
+        synchronized (beeperLocations) {
+            int[] location = {street, avenue};
+            beeperLocations.add(location);
+        }
     }
 
     public int getMaxBeepers() {
         return this.maxBeepers;
     }
 
-    public int[] getDeliveryPosition()
-    {
+    public int[] getDeliveryPosition() {
         return this.deliveryPosition;
     }
 
     public void run() {
         race();
+        System.out.println("I'm not gonna run away, I never go back on my word! That's my nindo: my ninja way! - Naruto Uzumaki");
+    }
+
+    // Obtener la ubicación actual
+    public int[] getCurrentPosition() {
+        return new int[]{currentStreet, currentAvenue};
+    }
+
+    // Actualizar la ubicación al moverse
+    private void updatePosition() {
+        if (facingNorth()) {
+            currentStreet++;
+        } else if (facingSouth()) {
+            currentStreet--;
+        } else if (facingEast()) {
+            currentAvenue++;
+        } else if (facingWest()) {
+            currentAvenue--;
+        }
+    }
+
+    // Mover a la derecha
+    public void moveRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        if (frontIsClear()) {
+            move();
+            updatePosition();
+        }
+    }
+
+    // Mover a la izquierda
+    public void moveLeft() {
+        turnLeft();
+        if (frontIsClear()) {
+            move();
+            updatePosition();
+        }
+    }
+
+    // Mover y actualizar posición
+    @Override
+    public void move() {
+        super.move();
+        updatePosition();
     }
 }
 
-// this class create the robots
-class RobotFactory implements  Directions {
+class RobotFactory implements Directions {
     public static Racer[] createRobots(String[] args) {
         int r = 1; // Valor por defecto
         boolean e = false;
@@ -61,26 +244,26 @@ class RobotFactory implements  Directions {
             }
         }
 
-        // Create robots
+        // Crear robots
         List<Color> colores = new ArrayList<>();
         colores.add(Color.blue);
         colores.add(Color.red);
         colores.add(Color.green);
         colores.add(Color.yellow);
 
-        int[] quantity_of_beepers = { 1, 2, 4, 8 };
+        int[] quantity_of_beepers = {1, 2, 4, 8};
         Racer[] racers = new Racer[r];
-        int[] start_position = { 3, 5, 6, 7 };
+        int[] start_position = {3, 5, 6, 7};
 
         if (e) {
-            // -e is a parameter so all robots have the same number of beepers
+            // -e es un parámetro así que todos los robots tienen el mismo número de beepers
             System.out.println("All robots have the same number of beepers: ");
             int n = quantity_of_beepers[(int) (Math.random() * quantity_of_beepers.length)];
             for (int i = 0; i < r; i++) {
                 racers[i] = new Racer(start_position[i], 2, East, 0, colores.get(i % colores.size()), n);
             }
         } else {
-            // -e is not a parameter so all robots have different number of beepers
+            // -e no es un parámetro así que todos los robots tienen diferentes números de beepers
             System.out.println("All robots have different number of beepers: ");
             for (int i = 0; i < r; i++) {
                 int n = quantity_of_beepers[(int) (Math.random() * quantity_of_beepers.length)];
@@ -88,7 +271,7 @@ class RobotFactory implements  Directions {
             }
         }
 
-        // Print all parameters of racers
+        // Imprimir todos los parámetros de los robots
         for (int i = 0; i < r; i++) {
             System.out.println("Robot " + i + " has " + racers[i].getMaxBeepers() + " beepers max.");
         }
@@ -98,7 +281,7 @@ class RobotFactory implements  Directions {
 
     public static void startRobots(Racer[] racers) {
         System.out.println('\n' + "Start the searching: ");
-        // Start robots
+        // Iniciar los robots
         Thread[] threads = new Thread[racers.length];
         for (int i = 0; i < racers.length; i++) {
             final int index = i;
@@ -108,15 +291,12 @@ class RobotFactory implements  Directions {
     }
 }
 
-//this class create the beepers in the map
-class BeepersFactory{
-
-    public static void generateBeepersRandomly(String[] args)
-    {
+class BeepersFactory {
+    public static void generateBeepersRandomly(String[] args) {
         int min_avenue = 3;
         int min_street = 1;
-        int range_street =  8 - min_street +1;
-        int range_aveneu =  10 - min_avenue +1;
+        int range_street = 8 - min_street + 1;
+        int range_avenue = 10 - min_avenue + 1;
         int r = 1;
 
         for (int i = 0; i < args.length; i++) {
@@ -133,19 +313,18 @@ class BeepersFactory{
                     return;
                 }
             }
-        int num_beepers = 10; //usar args para encontrar la cantidad de beepers
-        for (int j = 0; j <r*100; j++)
-        {
-            
-            int street = (int)(Math.random()*range_street + min_street);
-            int avenue = (int)(Math.random()*range_aveneu + min_avenue);
-            World.placeBeepers(street, avenue, 1 );
-            
+        }
+
+        int num_beepers = r * 100; // Usar args para encontrar la cantidad de beepers
+        for (int j = 0; j < num_beepers; j++) {
+            int street = (int) (Math.random() * range_street + min_street);
+            int avenue = (int) (Math.random() * range_avenue + min_avenue);
+            World.placeBeepers(street, avenue, 1);
+            Racer.addBeeperLocation(street, avenue);
         }
     }
+}
 
-}
-}
 public class MiPrimerRobot {
     public static void main(String[] args) {
         BeepersFactory.generateBeepersRandomly(args);
@@ -157,5 +336,4 @@ public class MiPrimerRobot {
             RobotFactory.startRobots(racers);
         }
     }
-
 }
